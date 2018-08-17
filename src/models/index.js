0028417c9f2b56f627a3f08832d4fac6784a477f
Maecenas Li <lx70716@gmail.com>
2018-08-17 10:25:02 +0800

    Update db initializing
    * Always alter reference NOT NULL and exit(1) on db initialize error

--- src/models/index.js
+++ src/models/index.js
@@ -11,20 +11,21 @@ const Bookmark = sequelize.import(__dirname + '/bookmark');
   try {
     if (sequelize.config.database.match(/_test$/)) {
       await sequelize.sync({ force: true });
-      let alterQueries = [
-        'ALTER TABLE `bookmark` MODIFY `project_id` CHAR(36) NOT NULL',
-        'ALTER TABLE `data` MODIFY `project_id` CHAR(36) NOT NULL',
-        'ALTER TABLE `dimension` MODIFY `data_id` CHAR(36) NOT NULL',
-        'ALTER TABLE `project` MODIFY `user_id` CHAR(36) NOT NULL'
-      ].map(query => {
-        sequelize.query(query);
-      });
-      await Promise.all(alterQueries);
     } else {
       await sequelize.sync({ force: false });
     }
+    let alterQueries = [
+      'ALTER TABLE `bookmark` MODIFY `project_id` CHAR(36) BINARY NOT NULL',
+      'ALTER TABLE `data` MODIFY `project_id` CHAR(36) BINARY NOT NULL',
+      'ALTER TABLE `dimension` MODIFY `data_id` CHAR(36) BINARY NOT NULL',
+      'ALTER TABLE `project` MODIFY `user_id` CHAR(36) BINARY NOT NULL'
+    ].map(query => {
+      sequelize.query(query);
+    });
+    await Promise.all(alterQueries);
   } catch (err) {
-    throw err;
+    console.log(err);
+    process.exit(1);
   }
 })();
 
